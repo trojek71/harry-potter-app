1f98af0eb50a70d601365d4182ad7745ce5af138
tomekr <tomekr@gmail.com>
2019-11-19 14:29:49 +0100

    Add MoviesList component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <movies-list />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MoviesList from "./components/MoviesList.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    MoviesList
   }
 };
 </script>
---
+++ src/components/MoviesList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div v-for="movie in movies" :key="movie.id">{{movie.id}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MoviesList",
+  data() {
+    return {
+      movies: [
+        {
+          id: "1d5e7496-7120-4c59-8c21-72c728a73f85",
+          title: "Harry Potter and the Order of the Phoenix",
+          director: "David Yates",
+          composer: "Nicholas Hopper",
+          release_date: "2007-07-11"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
